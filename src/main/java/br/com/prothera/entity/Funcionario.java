b6fa525291181d9e3a4522f4302d5df7beb1fbdc
Luan Alves <alvesluanpc@gmail.com>
2026-09-24 23:11:49 -0300

    Cria entidades Pessoa e Funcionario
    Modelagem inicial usando heranca e padrao de dominio anemico (Lombok getters e setters) para compatibilidade com padroes de mercado.

--- src/main/java/br/com/prothera/entity/Funcionario.java
+++ src/main/java/br/com/prothera/entity/Funcionario.java
@@ -2,15 +2,19 @@ package br.com.prothera.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
-@Getter
-@Setter
+@Getter 
+@Setter 
 public class Funcionario extends Pessoa {
 
     private BigDecimal salario;
-
     private String funcao;
 
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
 }
--- src/main/java/br/com/prothera/entity/Pessoa.java
+++ src/main/java/br/com/prothera/entity/Pessoa.java
@@ -1,15 +1,16 @@
 package br.com.prothera.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
 
-@Getter
+@Getter 
 @Setter
+@AllArgsConstructor
 public class Pessoa {
 
     private String nome;
-
     private LocalDate dataNascimento;
 
 }
